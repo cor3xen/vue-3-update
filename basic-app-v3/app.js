e9fa85dcaeaf3c209bf1074981ebd5135e7af30e
cor3xen@outlook.com <73563384+cor3xen@users.noreply.github.com>
2020-12-21 03:00:32 +0100

    update to work with Vue3

--- basic-app-v3/app.js
+++ basic-app-v3/app.js
@@ -1,20 +1,24 @@
-Vue.component('the-button', {
-  template: '<button @click="updateMessage">Click me</button>',
-  methods: {
-    updateMessage() {
-      this.$emit('update');
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'This works in Vue 2!'
+        };
     },
-  },
+    methods: {
+        changeMessage() {
+            this.message = 'Will it work in Vue 3?';
+        },
+    },
 });
 
-new Vue({
-  el: '#app',
-  data: {
-    message: 'This works in Vue 2!',
-  },
-  methods: {
-    changeMessage() {
-      this.message = 'Will it work in Vue 3?';
+app.component('the-button', {
+    emits: ['update'],
+    template: '<button @click="updateMessage">Click me</button>',
+    methods: {
+        updateMessage() {
+            this.$emit('update');
+        },
     },
-  },
 });
+
+app.mount('#app');
